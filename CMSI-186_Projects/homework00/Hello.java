c51842f408509fe25d45f54dce27d528e9636510
steinmatt <mstein12@lion.lmu.edu>
2018-01-15 21:05:34 -0800

    Update Hello.java

--- CMSI-186_Projects/homework00/Hello.java
+++ CMSI-186_Projects/homework00/Hello.java
@@ -1,7 +1,14 @@
-/* 
- * Common Hello World greeter file
+/** 
+ * Hello.java
+ * @ Author: Matthew Stein 
+ * @ Date: 1/15/18
+ * @ Purpose: To write first Java program  
+ * @ Notes: 
+ * @ Warnings: No input recieved. Output is constant. 
+ * @ Exceptions: No expcetions
  */ 
-public class HelloWorld {
+
+public class Hello {
   public static void main ( String[] args) {
     System.out.println("Hello, world!");
   }
