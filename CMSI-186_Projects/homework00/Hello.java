eaadc97509ac461f923094f2505d24dfca396e7c
steinmatt <mstein12@lion.lmu.edu>
2018-01-15 21:00:24 -0800

    Update Hello.java

--- CMSI-186_Projects/homework00/Hello.java
+++ CMSI-186_Projects/homework00/Hello.java
@@ -1,3 +1,8 @@
 /* 
- * Hello java file space 
+ * Common Hello World greeter file
  */ 
+public class HelloWorld {
+  public static void main ( String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
